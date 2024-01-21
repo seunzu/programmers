3de0935388aa04c4eef5bf3e4bb2ba604c263edf
zzoo0123 <0123suh@gmail.com>
2024-01-21 20:33:41 +0900

    Auto stash before rebase of "origin/main"

---
+++ 코딩 기초 트레이닝/Day 1 출력/대소문자 바꿔서 출력하기.py
@@ -0,0 +1,9 @@
+str = input()
+str = str.swapcase()
+print(str)
+
+# for i in str:
+#     if i.isupper() == True:
+#         print(i.lower(), end="")
+#     else:
+#         print(i.upper(), end="")
