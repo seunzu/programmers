a260efbc0b3e191c8801b48c50040ab088fe4123
zzoo0123 <0123suh@gmail.com>
2024-01-25 01:47:17 +0900

    Auto stash before rebase of "origin/main"

---
+++ 코딩 기초 트레이닝/Day 6 조건문, 반복문/마지막 두 원소.py
@@ -0,0 +1,11 @@
+a = list(map(int, input().split()))
+
+def solution(num_list):
+    l = len(num_list)
+    if num_list[l - 2] < num_list[l - 1]:
+        num_list.append(num_list[l - 1] - num_list[l - 2])
+    else:
+        num_list.append(num_list[l - 1] * 2)
+    return num_list
+
+print(solution(a))
