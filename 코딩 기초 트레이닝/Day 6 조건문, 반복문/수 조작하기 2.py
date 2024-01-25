83b5221aa4962dc2dc43e11c5caa3735704cbce1
zzoo0123 <0123suh@gmail.com>
2024-01-25 23:44:58 +0900

    Auto stash before rebase of "origin/main"

---
+++ 코딩 기초 트레이닝/Day 6 조건문, 반복문/수 조작하기 2.py
@@ -0,0 +1,18 @@
+nlist = input().split()
+
+
+def solution(numLog):
+    answer = ''
+
+    for i in range(1, len(numLog)):
+        if numLog[i] - numLog[i - 1] == 1:
+            answer += 'w'
+        if numLog[i - 1] - numLog[i] == 1:
+            answer += 's'
+        if numLog[i] - numLog[i - 1] == 10:
+            answer += 'd'
+        if numLog[i - 1] - numLog[i] == 10:
+            answer += 'a'
+    return answer
+
+print(solution(nlist))
