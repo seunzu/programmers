ea6f303b7b8ab813a484dea0dc7a36ac0de2c616
zzoo0123 <0123suh@gmail.com>
2024-01-31 14:00:16 +0900

    Auto stash before rebase of "origin/main"

--- 코딩테스트 입문/Day 10 조건문, 배열, 수학, 시뮬레이션/배열 회전시키기.py
+++ 코딩테스트 입문/Day 10 조건문, 배열, 수학, 시뮬레이션/배열 회전시키기.py
@@ -1,10 +1,21 @@
+# def solution(numbers, direction):
+#     answer = []
+#     if direction == 'right':
+#         answer = [numbers[-1]] + numbers[:-1]
+#     elif direction == 'left':
+#         answer = numbers[1:] + [numbers[0]]
+#     return answer
+
+from collections import deque
+
 def solution(numbers, direction):
-    answer = []
+    numbers_deque = deque(numbers)
+
     if direction == 'right':
-        answer = [numbers[-1]] + numbers[:-1]
+        numbers_deque.rotate(1)
     elif direction == 'left':
-        answer = numbers[1:] + [numbers[0]]
-    return answer
+        numbers_deque.rotate(-1)
+    return list(numbers_deque)
 
 a = list(map(int, input().split()))
 b = input()
