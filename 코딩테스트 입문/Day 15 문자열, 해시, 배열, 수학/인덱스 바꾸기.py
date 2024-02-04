1dda833973e4618680c5664fdf39049a578e1768
zzoo0123 <0123suh@gmail.com>
2024-02-04 14:50:03 +0900

    Auto stash before rebase of "origin/main"

--- 코딩테스트 입문/Day 15 문자열, 해시, 배열, 수학/인덱스 바꾸기.py
+++ 코딩테스트 입문/Day 15 문자열, 해시, 배열, 수학/인덱스 바꾸기.py
@@ -1,3 +1,9 @@
 def solution(my_string, num1, num2):
-    answer = ''
-    return answer
+    my_string = list(my_string)
+    my_string[num1], my_string[num2] = my_string[num2], my_string[num1]
+    return ''.join(my_string)
+
+s = list(input())
+a, b = map(int, input().split())
+
+print(solution(s, a, b))
